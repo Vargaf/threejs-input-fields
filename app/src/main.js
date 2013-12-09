ce47aa4ccaec966b55a7007321b00a28085ba6be
Fernando <fernandovillarp@gmail.com>
2013-12-09 09:52:58 +0100

    Experimenting with fullscreen

--- app/src/main.js
+++ app/src/main.js
@@ -59,7 +59,7 @@ require(
         var orthographicElement = true;
         var animationController = new AnimationControllerClass();
 
-        var mainCanvas = new CanvasElementClass( { el: canvasContent, 'canvasWidth': canvasWidth, 'canvasHeight': canvasHeight } );
+        mainCanvas = new CanvasElementClass( { el: canvasContent, 'canvasWidth': canvasWidth, 'canvasHeight': canvasHeight } );
 
         var exampleObjects = new ExampleObjectsClass();
         mainCanvas.add( exampleObjects.getObjects() );
@@ -177,6 +177,81 @@ require(
         function concatenateValueToOutput( event ) {
 
             outputField.setValue( outputField.getInputValue() + ' -- ' + event.object.getInputValue() );
+
+        }
+
+        // http://www.html5rocks.com/en/tutorials/pointerlock/intro/
+
+        var havePointerLock = 'pointerLockElement' in document || 'mozPointerLockElement' in document || 'webkitPointerLockElement' in document;
+
+        if ( havePointerLock ) {
+
+            var element = mainCanvas.getRenderer().domElement;
+
+            var pointerlockchange = function ( event ) {
+
+                if ( document.pointerLockElement === element || document.mozPointerLockElement === element || document.webkitPointerLockElement === element ) {
+
+                    //controls.enabled = true;
+
+                } else {
+
+                    //controls.enabled = false;
+
+                }
+
+            }
+
+            var pointerlockerror = function ( event ) {
+
+            }
+
+            // Hook pointer lock state change events
+            document.addEventListener( 'pointerlockchange', pointerlockchange, false );
+            document.addEventListener( 'mozpointerlockchange', pointerlockchange, false );
+            document.addEventListener( 'webkitpointerlockchange', pointerlockchange, false );
+
+            document.addEventListener( 'pointerlockerror', pointerlockerror, false );
+            document.addEventListener( 'mozpointerlockerror', pointerlockerror, false );
+            document.addEventListener( 'webkitpointerlockerror', pointerlockerror, false );
+
+            mainCanvas.getRenderer().domElement.addEventListener( 'click', function ( event ) {
+
+                // Ask the browser to lock the pointer
+                element.requestPointerLock = element.requestPointerLock || element.mozRequestPointerLock || element.webkitRequestPointerLock;
+
+                if ( /Firefox/i.test( navigator.userAgent ) ) {
+
+                    var fullscreenchange = function ( event ) {
+
+                        if ( document.fullscreenElement === element || document.mozFullscreenElement === element || document.mozFullScreenElement === element ) {
+
+                            document.removeEventListener( 'fullscreenchange', fullscreenchange );
+                            document.removeEventListener( 'mozfullscreenchange', fullscreenchange );
+
+                            element.requestPointerLock();
+                        }
+
+                    }
+
+                    document.addEventListener( 'fullscreenchange', fullscreenchange, false );
+                    document.addEventListener( 'mozfullscreenchange', fullscreenchange, false );
+
+                    element.requestFullscreen = element.requestFullscreen || element.mozRequestFullscreen || element.mozRequestFullScreen || element.webkitRequestFullscreen;
+
+                    element.requestFullscreen();
+
+                } else {
+
+                    element.requestPointerLock();
+
+                }
+
+            }, false );
+
+        } else {
+
+            mainCanvas.getRenderer().domElement.removeEventListener( 'click' );
 
         }
 
